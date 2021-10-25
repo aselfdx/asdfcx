e989e6e49368785176a479d8b8fea1d7b0687258
aselfdx <93139186+aselfdx@users.noreply.github.com>
2021-10-25 16:52:20 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,114 @@
+<script id="_wauxdh">var _wau = _wau || [];
+_wau.push(["tab", "fhibrymuplv0", "xdh", "left-middle"]);
+(function() {var s=document.createElement("script"); s.async=true;
+s.src="//widgets.amung.us/tab.js";
+document.getElementsByTagName("head")[0].appendChild(s);
+})();</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>İnstaProTakip - İnstagram Şifresiz Beğeni</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="İnstagram şifresiz sınırsız beğeni hilesi mevcuttur.Yakında instagram sınırsız takipçi hilesi eklenecektir.Beklemede Kalın - Arda Tekinalp ">
+  <meta name="keywords" content="İnstagram beğeni hilesi , instagram şifresiz beğeni , instagram beğeni , instagram ücretsiz beğeni , instagram beğenileri , instagram beğeni almak , instagram ücretsiz beğeni almak , instagram ücretsiz fotoğraf beğendirmek , instagram fotoğraf beğendirme , spotify generator , spotify account generator , spotify premium generator , spotify account generator premium , ücretsiz spotify account , ücretsiz spotify oluşturucu , minecraft premium account , minecraft premium account generator , minecraft mc generator , account generator , Arda Tekinalp, şifresiz instagram beğeni, instagram beğeni">
+  <meta name="author" content="Arda Tekinalp Şifresiz Sınırsız Beğeni">
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content="Ücretsiz İnstagram Sınırsız Takipçi ve Beğeni Hilesi" />
+  <meta property="og:description" content="İnstagram Şifresiz Beğeni , İnstagram Beğeni Scripti, İnstagram Scripti, Arda Tekinalp, Arda Tekinalp Beğeni Gönderme"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+  <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-4252216698604251",
+    enable_page_level_ads: true
+  });
+</script>
+</head>
+<body id="#" data-spy="scroll" data-target=".navbar" data-offset="60">
+<?php  if (!empty($_REQUEST['url'])) { $sayfa = $_REQUEST['url']; echo "<script>window.location = 'ardatknlp.php?id=$sayfa';</script>"; } ?>
+<div class="jumbotron text-center">
+  <h1>İnstagram Şifresiz Sınırsız Beğeni</h1> 
+  <p class="text-warning">Bu Sistem Sizlere Arda Tekinalp Tarafından Ücretsiz Bir Şekilde Sunuldu! Sistemin Çalınıp Kullanılması Halinde Yasal İşlem Başlatılacaktır!</p> 
+  <p class="text-danger">Sistemin Çalışmadığını İddaa Edenler Varsa İnstagram'dan Yazınız veya Altta Bulunan Bölümden Açıklamayı Okuyunuz! <br>İnstagram:<a href="http://instagram.com/ardatknlp" target="_blank">@ardatknlp</a></p>
+  <p>Beğeni Göndermiyor ?<a data-toggle="modal" data-target="#NedenCalismiyor"> <font size="5" color="red"> Tıkla </font></h4></a>
+  <div class="text-center" class="input-group">
+  <a class="btn btn-primary" href="http://instaprotakip.com/down/" target="_blank" role="button">Youtube MP4 İndirmek İçin Tıklayın!</a>
+  <a class="btn btn-danger" href="http://instaprotakip.com" target="_blank" role="button">İnstagram Araçları</a>
+  <a class="btn btn-warning" href="https://instagram.com/error.aspx" target="_blank" role="button">Admin İnstagram</a>
+  <a class="btn btn-info" href="https://instagram.com/error.aspx" target="_blank" role="button">Scripti Satın Almak İçin Tıkla</a>
+  <br>
+  <br>
+   <form>
+    <div class="input-group">
+	<form action="ardatknlp.php" method="POST">
+      <input type="url" name="url" id="url"  class="form-control" size="50" placeholder="Post URL'si" required>
+      <div class="input-group-btn">
+        <button type="submit" class="btn btn-success">GÖNDER</button>
+      </div>
+	</form>
+    </div>
+  </form>
+<br>
+<br> 
+ <footer class="container-fluid text-center">
+  <a href="#" title="Yukarı Git">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <p class="text-muted"><font color="white">Script by </font><a href="https://instagram.com/error.aspx" target="_blank">Arda Tekinalp</a></p>
+</footer>
+<!-- urlbildir.com - sayac kodu -->
+<script type="text/javascript" src="http://urlbildir.com/counter.php"></script>
+<!-- urlbildir.com - sayac kodu -->
+  <!-- toplist35.xyz - sayac kodu -->
+<script type="text/javascript" src="http://www.toplist35.xyz/counter.php"></script>
+<!-- toplist35.xyz - sayac kodu -->
+  <!-- iyi.tc - sayac kodu -->
+<script type="text/javascript" src="http://iyi.tc/counter.php"></script>
+<!-- iyi.tc - sayac kodu -->
+<!-- hitbildir.com - Sayac Kodu -->
+<script type="text/javascript" src="http://hitbildir.com/counter.php"></script>
+<!-- hitbildir.com - Sayac Kodu -->
+<!-- doruk100.com - sayac kodu -->
+<script type="text/javascript" src="http://www.doruk100.com/sayac.php"></script>
+<!-- doruk100.com - sayac kodu -->
+  </div>
+  </div>
+<div class="control-sidebar-bg"></div>
+</div>
+<div id="NedenCalismiyor" class="modal fade" role="dialog">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal">&times;</button>
+<h4 class="modal-title">Neden Beğeni Gelmiyor ?</h4>
+</div>
+<div class="modal-body">
+Merhabalar Beğeni Göndermiyor ise Birçok Nedeni Olabilir!</br></br>
+1. Neden : Eski Bir Fotoğrafa Beğeni Göndermeye Çalışıyorsanız Olmaz! Yeni Bir Fotoğraf Paylaşarak Deneyin!.
+</br></br>
+2. Neden : Fotoğraf Linkinizin Sonunda / veya ?taken-by=ardatknlp Gibi Böyle Bir Yazı Olmamalıdır!
+</br></br>
+Yanlış Fotoğraf Linki :<font size="2" color="red"> https://www.instagram.com/p/BbUS8jpnAD2/?taken-by=ardatknlp </font>
+</br></br>
+Doğru Fotoğraf Linki : <font size="2" color="green">https://www.instagram.com/p/BbUS8jpnAD2</font>
+</br></br>
+3. Neden : Fotoğrafınız +18 Bir Fotoğraf İse Beğeni Göndermeyebilir.
+</br></br>
+4. Neden : Beğeniler Çok Yavaş Geliyor. Site'de Ne Kadar Fazla Durursanız o Kadar Fazla Beğeni Gelir.
+</br></br>
+5. Neden : Herşeyi Yaptım Fakat Beğeni Göndermiyor Mobildeyim. Mobil'de Kopyaladığınız Linkler Yanlış Olabilir.
+</br></br>
+6. Neden : 1-2 Dakika Bekledim Beğeni Gelmedi . Sitede 10 Dakika Beklemeyi Deneyiniz.
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
